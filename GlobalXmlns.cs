0ce0f48be1e1e6c9d3b42db5d2d5d618bf9262c1
Tomáš Klepač <74875406+tomasklepac@users.noreply.github.com>
2025-11-19 23:54:56 +0100

    Initial commit client

---
+++ GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "PiskvorkyClientGUI")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "PiskvorkyClientGUI.Pages")]
---
+++ MauiProgram.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace PiskvorkyClientGUI
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+#if DEBUG
+            builder.Logging.AddDebug(); // Přidá logování do Debug výstupu
+#endif
+
+            return builder.Build();
+        }
+    }
+}
